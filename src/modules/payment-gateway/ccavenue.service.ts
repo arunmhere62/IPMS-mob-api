79b970d092806d707e955ed5f641fc85c39c7fee
arunmhere62 <arunmhere62@gmail.com>
2026-07-19 02:41:32 +0530

    cc fix

--- src/modules/payment-gateway/ccavenue.service.ts
+++ src/modules/payment-gateway/ccavenue.service.ts
@@ -86,7 +86,7 @@ export class CCavenueService {
   encrypt(plainText: string): string {
     try {
       const key = this.getAESKey();
-      const iv = Buffer.from([0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f]);
+      const iv = Buffer.from('0123456789abcdef', 'utf8');
       const cipher = crypto.createCipheriv('aes-128-cbc', key, iv);
       let encoded = cipher.update(plainText, 'utf8', 'hex');
       encoded += cipher.final('hex');
@@ -103,7 +103,7 @@ export class CCavenueService {
   decrypt(encText: string): string {
     try {
       const key = this.getAESKey();
-      const iv = Buffer.from([0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f]);
+      const iv = Buffer.from('0123456789abcdef', 'utf8');
       const decipher = crypto.createDecipheriv('aes-128-cbc', key, iv);
       let decoded = decipher.update(encText, 'hex', 'utf8');
       decoded += decipher.final('utf8');
--- src/modules/subscription/subscription.service.ts
+++ src/modules/subscription/subscription.service.ts
@@ -723,7 +723,7 @@ export class SubscriptionService {
   private ccavenueEncrypt(plainText: string): string {
     try {
       const key = this.getAESKey();
-      const iv = Buffer.from([0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f]);
+      const iv = Buffer.from('0123456789abcdef', 'utf8');
       
       const cipher = crypto.createCipheriv('aes-128-cbc', key, iv);
       let encrypted = cipher.update(plainText, 'utf8', 'hex');
@@ -742,7 +742,7 @@ export class SubscriptionService {
   private ccavenueDecrypt(encryptedText: string): string {
     try {
       const key = this.getAESKey();
-      const iv = Buffer.from([0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f]);
+      const iv = Buffer.from('0123456789abcdef', 'utf8');
       
       const decipher = crypto.createDecipheriv('aes-128-cbc', key, iv);
       let decrypted = decipher.update(encryptedText, 'hex', 'utf8');

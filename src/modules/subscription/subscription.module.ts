9aecbb79da92e52ef1320b2e10b5ad03b8cb5284
arunmhere62 <arunmhere62@gmail.com>
2026-08-17 12:40:35 +0530

    api

--- src/modules/subscription/subscription.module.ts
+++ src/modules/subscription/subscription.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
 import { SubscriptionController } from './subscription.controller';
 import { SubscriptionService } from './subscription.service';
+import { IapService } from './iap.service';
 import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [AuthModule],
   controllers: [SubscriptionController],
-  providers: [SubscriptionService, PrismaService],
-  exports: [SubscriptionService],
+  providers: [SubscriptionService, IapService, PrismaService],
+  exports: [SubscriptionService, IapService],
 })
 export class SubscriptionModule {}
